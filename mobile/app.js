d6e95694b7175eb07a6b5e0fb2fa3ae9080912ad
MongusChoi <Mongus.C@gmail.com>
2017-05-09 17:42:49 +0900

    회원가입 부분 추가

--- mobile/app.js
+++ mobile/app.js
@@ -4,6 +4,12 @@ var bodyParser = require('body-parser');
 var underscore = require('underscore');
 var mysql = require('./database/mysql');  //mysql 선언부, 상대경로로 선언된 부분
 
+var login = require=('./user/login.js');
+var join = require=('./user/join.js');
+
+app.post('/user/login', login.login);
+app.post('/user/join', join.join);
+
 app.listen(3000, function() {
   console.log('Connection 3000 port!');
 })
